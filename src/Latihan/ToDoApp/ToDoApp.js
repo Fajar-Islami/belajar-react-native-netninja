13289789fb97964e24aa99d98074b96d0f831bdc
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-01-07 22:49:49 +0700

    Video 10 ToDoApp Part 2

--- src/Latihan/ToDoApp/ToDoApp.js
+++ src/Latihan/ToDoApp/ToDoApp.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
 import Header from './Component/Header';
+import ToDoItem from './Component/ToDoItem';
 
 export default function ToDoApp() {
   const [todos, setTodos] = useState([
@@ -8,6 +9,13 @@ export default function ToDoApp() {
     {text: 'create an app', key: '2'},
     {text: 'play on the switch', key: '3'},
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header />
@@ -16,7 +24,9 @@ export default function ToDoApp() {
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({item}) => <Text>{item.text}</Text>}
+            renderItem={({item}) => (
+              <ToDoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
