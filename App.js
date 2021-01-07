470e72a5473534aaf462e9eb4103018e9c9693b0
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-01-07 16:51:14 +0700

    Video 4 State

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Fajar');
+  const [person, setPerson] = useState({name: 'Ahmad', age: 40});
+
+  const clickHandler = () => {
+    setName('Ahmad');
+    setPerson({name: 'Fajar', age: 20});
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello phone</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>Lorem Ipsu dolor sit amet.</Text>
-        <Text>Lorem Ipsu dolor sit amet.</Text>
-        <Text>Lorem Ipsu dolor sit amet.</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
       </View>
     </View>
   );
@@ -23,15 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20,
+  buttonContainer: {
+    marginTop: 20,
   },
 });
